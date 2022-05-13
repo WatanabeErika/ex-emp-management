371be9663df865870cb047f31974920147152c02
watanabe erika <erika.watanabe@rakus-partners.co.jp>
2022-05-13 17:36:00 +0900

    emp/result

--- src/main/java/jp/co/sample/controller/EmployeeController.java
+++ src/main/java/jp/co/sample/controller/EmployeeController.java
@@ -5,13 +5,20 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import jp.co.sample.domain.Employee;
+import jp.co.sample.form.UpdateEmployeeForm;
 import jp.co.sample.service.EmployeeService;
 
 @Controller
 @RequestMapping("/employee")
 public class EmployeeController {
+	@ModelAttribute
+	private UpdateEmployeeForm setUpUpdateEmployeeForm() {
+		return new UpdateEmployeeForm();
+	}
+	
 	@Autowired
 	private EmployeeService employeeService;
 
@@ -20,5 +27,12 @@ public class EmployeeController {
 		List<Employee> employeeList = employeeService.showList();
 		model.addAttribute("employeeList", employeeList);
 		return "employee/list";
+	}
+	
+	@RequestMapping("/showDetail")
+	public String showDetail(String id,Model model) {
+		Employee employee=employeeService.showDetail(Integer.parseInt(id));
+		model.addAttribute("employee", employee);
+		return "employee/detail";
 	}
 }
