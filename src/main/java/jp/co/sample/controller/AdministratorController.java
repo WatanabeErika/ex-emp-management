3ef3cdf1a79dc30537d712a21890c2d514beb288
watanabe erika <erika.watanabe@rakus-partners.co.jp>
2022-05-13 15:11:04 +0900

    login-result

--- src/main/java/jp/co/sample/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/controller/AdministratorController.java
@@ -1,8 +1,11 @@
 package jp.co.sample.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -16,6 +19,9 @@ import jp.co.sample.service.AdministratorService;
 public class AdministratorController {
 	@Autowired
 	private AdministratorService administratorService;
+	
+	@Autowired
+	private HttpSession session;
 	
 	@ModelAttribute
 	public InsertAdministratorForm setUpInsertAdministratorForm() {
@@ -43,5 +49,18 @@ public class AdministratorController {
 	@RequestMapping("/")
 	public String toLogin() {
 		return "administrator/login";
+	}
+	
+	@RequestMapping("/login")
+	public String login(LoginForm form,Model model) {
+		Administrator admin=administratorService.login(form.getMailAddress(),form.getPassword());
+		
+		if(form==null) {
+			model.addAttribute("fail", "メールアドレスまたはパスワードが不正です。");
+			return "login";
+		}else {
+			session.setAttribute("administratorName", admin);
+			return "forward:/employee/showList";
+		}
 	}
 }
